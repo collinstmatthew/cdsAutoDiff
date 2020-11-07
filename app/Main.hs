c337b5c158fd699e5322142ba1814af68ab5e374
collinstmatthew <collins.t.matthew@protonmail.com>
2020-11-07 18:13:08 +0000

    price evolution working

--- app/Main.hs
+++ app/Main.hs
@@ -5,17 +5,29 @@ import Numeric.Backprop
 import Debug.Trace
 
 -- bs auto modules
-import Market(SimpleMarket(..),Curve(..))
+import Market(SimpleMarket(..),Curve(..),diffMarket,addMarket,divideMarket)
 import Trades.CashFlow(CashFlows(..))
 import Trades.CallOption(callPrice,ModelParams(..))
 import Trades.CDS(Credit(..),cdsPrice)
 import Types
 
+import Data.List(iterate)
+
+-- allow it so you can add two markets together with a type class
+-- what to do if nodes are different? Take union of all nodes I guess
+-- so first take difference between nodes, divide by n and then keep adding on
+
 -- takes a starting market and an ending market and evoles the price linearly
 -- using n number of points
 -- at the moment just gives back the price at each market
-evolveLinear :: CashFlows -> Credit -> SimpleMarket -> SimpleMarket -> Int -> Price
-evolveLinear fl cd mktStart mktEnd n = evalBP (cdsPrice fl cd) mktStart
+--
+
+evolveLinear :: CashFlows -> Credit -> SimpleMarket -> SimpleMarket -> Int -> [(SimpleMarket,SimpleMarket,Price)]
+evolveLinear fl cd mktStart mktEnd n = zip3 allMkts grads prices where
+    intermediateMkt  = divideMarket (fromIntegral n) $ diffMarket mktEnd mktStart
+    allMkts          = take (n+2) $ iterate (addMarket intermediateMkt) mktStart
+    prices           = map (evalBP (cdsPrice fl cd)) allMkts
+    grads            = map (gradBP (cdsPrice fl cd)) allMkts
 
 
 main :: IO ()
@@ -27,24 +39,26 @@ main = do
 
     -- Create out market from curves
     -- These are market forward rates
-    let irCurve      = Curve [0.1,0.5,1,1.5,2] [0.5,0.5,0.5,0.5,0.5]
-    --let hazardRates  = Curve [0.1,0.5,1,1.5,2] [0.01,0.05,0.02,0.025,0.03]
-    let hazardRates  = Curve [0.1,0.5,1,1.5,2] [0.01,0.05,0.02,0.025,0.03]
+    let irCurve1      = Curve [0.1,0.5,1,1.5,2] [0.5,0.5,0.5,0.5,0.5]
+        hazardRates1  = Curve [0.1,0.5,1,1.5,2] [0.01,0.05,0.02,0.025,0.03]
+        mkt1 = SimpleMarket irCurve1 hazardRates1
 
-    -- create an interest rate curve
-    let mkt = SimpleMarket irCurve hazardRates
+
+    let irCurve2      = Curve [0.1,0.5,1,1.5,2] [0.8,0.8,0.8,0.8,0.8]
+        hazardRates2  = Curve [0.1,0.5,1,1.5,2] [0.01,0.05,0.02,0.025,0.03]
+        mkt2 = SimpleMarket irCurve2 hazardRates2
 
     -- create cashflows for fixed leg
     let fixedLegCashFlow = CashFlows [0.05,1,1.5,2] [2,3,4,5]
     -- create credit data notional and recovery rate
     let creditData       = Credit 10 0.4
 
-    let result = gradBP (cdsPrice fixedLegCashFlow creditData) mkt
-    let result' = evolveLinear fixedLegCashFlow creditData mkt mkt 5
+    let result = gradBP (cdsPrice fixedLegCashFlow creditData) mkt1
+    let result' = evolveLinear fixedLegCashFlow creditData mkt1 mkt2 3
     print result'
 
     --print $ evalBP2 integrateCurve hazardRates 3
-    print $ result
+--    print $ result
 --    print $ evolveLinear fixedLegCashFlow creditData mkt
 
     print "finished"
--- src/Market.hs
+++ src/Market.hs
@@ -14,7 +14,11 @@ module Market(SimpleMarket(..),
               integrateCurve,
               nodeDates,
               getVal,
-              yieldCurve) where
+              yieldCurve,
+              diffMarket,
+              addMarket,
+              divideMarket
+             ) where
 
 import Types
 
@@ -35,12 +39,59 @@ data Curve = Curve { _dates :: [Time],
 instance Backprop Curve
 makeLenses ''Curve
 
+
+nodeDates' :: Curve -> Curve -> [Time]
+nodeDates' c1 c2 = uniqueSort (d1 ++ d2) where
+    d2    = view dates c1
+    d1    = view dates c2
+
+
+-- get the appropriate value at the speicifc time assuming piecewise constant
+getVal' :: Curve -> Time -> Rate
+getVal' curve time  | null together = last ratesG
+                    | otherwise    = (snd . head) together where
+    together = dropWhile (\x -> (fst x) < time)  $ zip datesG ratesG
+    datesG = view dates curve
+    ratesG = view rates curve
+
+-- find the difference of the two curves
+diffCurve :: Curve -> Curve -> Curve
+diffCurve c1 c2 = Curve dates1 rates1 where
+    dates1 = nodeDates' c1 c2
+    rates1 = map (\x -> getVal' c1 x - getVal' c2 x) dates1
+
+-- find the difference of the two curves
+addCurve :: Curve -> Curve -> Curve
+addCurve c1 c2 = Curve dates1 rates1 where
+    dates1 = nodeDates' c1 c2
+    rates1 = map (\x -> getVal' c1 x + getVal' c2 x) $ dates1
+
+-- # TODO can make this better using the lenses
+divideCurve :: Double -> Curve -> Curve
+divideCurve m c1 = Curve (view dates c1) (map (/m) (view rates c1))
+
+
 -- Market object is made up of iterest and hazard rates
 data SimpleMarket = SimpleMarket { _irCurve     :: Curve,
                                    _hazardRates :: Curve } deriving (Show,Generic)
 
 instance Backprop SimpleMarket
 makeLenses ''SimpleMarket
+
+diffMarket :: SimpleMarket -> SimpleMarket -> SimpleMarket
+diffMarket m1 m2 = SimpleMarket ir hc where
+    ir = diffCurve (view irCurve m1) (view irCurve m2)
+    hc = diffCurve (view hazardRates m1) (view hazardRates m2)
+
+addMarket :: SimpleMarket -> SimpleMarket -> SimpleMarket
+addMarket m1 m2 = SimpleMarket ir hc where
+    ir = addCurve (view irCurve m1) (view irCurve m2)
+    hc = addCurve (view hazardRates m1) (view hazardRates m2)
+
+divideMarket :: Double -> SimpleMarket -> SimpleMarket
+divideMarket m mkt = SimpleMarket ir hc where
+    ir = divideCurve m (view irCurve mkt)
+    hc = divideCurve m (view hazardRates mkt)
 
 -- take forward rates and gives a discount factor back
 -- assums forward rates are piecewise constant
@@ -55,6 +106,8 @@ integrateCurve forwardRates t = exp (-(dot timesDiff forwardRates')) where
     filteredT     = filter (t >) datesH
     times'        = 0 : init times
     timesDiff     = difference (Just 0) times
+
+
 
 nodeDates :: Reifies s W => BVar s SimpleMarket -> [BVar s Time]
 nodeDates mkt = uniqueSort (irDates ++ hazDates) where
